73e1e0dc51433952057a1fa764e45511e33675b0
DevVigilante <96680248+DevVigilante@users.noreply.github.com>
2026-04-03 18:11:34 +0530

    fix: use tar source files to skip extra folder directory

--- packages/zlib/build.sh
+++ packages/zlib/build.sh
@@ -1,6 +1,5 @@
 HAS_SOURCE=false
-SRC_URL=https://github.com/madler/zlib/releases/download/v1.3.2/zlib132.zip
-SRC_DIR=zlib-1.3.2
+SRC_URL=https://github.com/madler/zlib/releases/download/v1.3.2/zlib-1.3.2.tar.xz
 
 build_package() { 
     echo "Building $PACKAGE"
