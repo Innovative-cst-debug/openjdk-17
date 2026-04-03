700cbcf34ed3ebbe2ce7428d77774c03064fe366
DevVigilante <96680248+DevVigilante@users.noreply.github.com>
2026-04-03 18:39:46 +0530

    feat: setup zlib for complete build and some enhancement

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+PACKAGE_NAME=com.logicodeum.ide
+PREFIX=/data/data/$PACKAGE_NAME/files/usr
+
 source ./setup.sh
 source ./download_source.sh
 source ./apply_source_patch.sh
@@ -10,18 +13,48 @@ function unset_variables {
     unset PACKAGE
     unset PACKAGE_DIRECTORY
     unset BUILD_SCRIPT
+    unset ARCH
+    unset TARGET
+    unset API
+    unset NDK
+    unset TOOLCHAIN
+    unset SRC_URL
+}
+
+function configure_and_build {
+  export ARCH=$1
+
+  case $ARCH in
+    arm64)
+      export TARGET=aarch64-linux-android
+      export API=24
+      ;;
+    arm)
+      export TARGET=armv7a-linux-androideabi
+      export API=21
+      ;;
+    x86)
+      export TARGET=i686-linux-android
+      export API=21
+      ;;
+    x86_64)
+      export TARGET=x86_64-linux-android
+      export API=24
+      ;;
+  esac
+  configure
+  build_package
 }
 
 export PACKAGE=$1
+PACKAGE_DIRECTORY="$(pwd)/packages/$PACKAGE"
+BUILD_SCRIPT="$PACKAGE_DIRECTORY/build.sh"
 
 if [ -z "$PACKAGE" ]; then
     echo "Usage: ./build.sh <package>"
     unset_variables
     exit 1
 fi
-
-PACKAGE_DIRECTORY="$(pwd)/packages/$PACKAGE"
-BUILD_SCRIPT="$PACKAGE_DIRECTORY/build.sh"
 
 if [ ! -f "$BUILD_SCRIPT" ]; then
     echo "Package not found: $PACKAGE"
@@ -36,8 +69,15 @@ if ! $HAS_SOURCE; then
     download_and_extract_to_src $SRC_URL
 fi
 
+# Apply patches
 apply_patches
-build_package
+
+# Build for all architecture
+cd "$PACKAGE_DIRECTORY/src"
+for arch in arm64 arm x86 x86_64; do
+  configure_and_build $arch
+done
+cd ../..
 
 unset_variables
 
--- packages/zlib/build.sh
+++ packages/zlib/build.sh
@@ -1,6 +1,42 @@
 HAS_SOURCE=false
 SRC_URL=https://github.com/madler/zlib/releases/download/v1.3.2/zlib-1.3.2.tar.xz
 
+configure() { 
+    echo "[*] Configuring build for $PACKAGE-$ARCH"
+    NDK=$ANDROID_NDK
+    TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64
+    export CC="$TOOLCHAIN/bin/${TARGET}${API}-clang --sysroot=$TOOLCHAIN/sysroot"
+    export CXX="$TOOLCHAIN/bin/${TARGET}${API}-clang++ --sysroot=$TOOLCHAIN/sysroot"
+    export AR=$TOOLCHAIN/bin/llvm-ar
+    export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
+    export STRIP=$TOOLCHAIN/bin/llvm-strip
+    export CFLAGS="-fPIC"
+    export LDFLAGS=""
+}
+
 build_package() { 
-    echo "Building $PACKAGE"
+    echo "[*] Building $PACKAGE-$ARCH"
+    
+    BUILD_DIR="build-$ARCH"
+    rm -rf "$BUILD_DIR"
+    mkdir -p "$BUILD_DIR"
+    cd "$BUILD_DIR"
+  
+    ../configure \
+      --prefix=$PWD/build/$PREFIX \
+      --shared \
+      --uname=Linux
+  
+    make -j$(nproc)
+    make install
+    cd ..
+    
+    # Unset all toolchain and flags
+    unset CC
+    unset CXX
+    unset AR
+    unset RANLIB
+    unset STRIP
+    unset CFLAGS
+    unset LDFLAGS
 }
