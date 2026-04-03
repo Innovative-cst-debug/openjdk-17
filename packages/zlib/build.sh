dd87756f695e604f2e23cf14ff6a96790d897df1
DevVigilante <96680248+DevVigilante@users.noreply.github.com>
2026-04-03 17:21:17 +0530

    script: add has source variable for source dowloading checks

--- packages/zlib/build.sh
+++ packages/zlib/build.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
-set -e
-
-HAS_SOURCE=true
+HAS_SOURCE=false
 SRC_URL=https://github.com/madler/zlib/releases/download/v1.3.2/zlib132.zip
 SRC_DIR=zlib-1.3.2
 
 build_package() { 
-    
-
+    echo "Building $PACKAGE"
 }
